02a9bb7e098016d78ffb9994177ce1d8ac3d2107
caglar bilgin <caglar.bilgin@testinium.com>
2023-01-13 22:14:55 +0300

    create a integration test without method

--- src/main/java/com/task/account/AccountApplication.java
+++ src/main/java/com/task/account/AccountApplication.java
@@ -5,25 +5,55 @@ import com.task.account.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+import java.time.Clock;
 import java.util.HashSet;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class AccountApplication implements CommandLineRunner {
 
-	private final CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
-	public AccountApplication(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
+    public AccountApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Customer customer = customerRepository.save(new Customer("","caglar","bilgin", new HashSet<>()));
-		System.out.println(customer);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Customer customer = customerRepository.save(new Customer("", "caglar", "bilgin", new HashSet<>()));
+        System.out.println(customer);
+    }
+
+    @ConditionalOnProperty(
+            prefix = "command.line.runner",
+            value = "enabled",
+            havingValue = "true",
+            matchIfMissing = true
+    )//Bu beanler sadece yukarıdaki condition'a uyduğu zaman yaratılsın diye ConditionalOnProperty ekledik
+    @Component
+    public class CommandLineTaskExecutor implements CommandLineRunner {
+        @Bean
+        public Clock clock() {
+            return Clock.systemUTC();
+        }
+
+        @Bean
+        public Supplier<UUID> uuidSupplier() {
+            return UUID::randomUUID;
+        }
+
+        @Override
+        public void run(String... args) throws Exception {
+
+        }
+    }
 }
--- src/test/java/com/task/account/controller/AccountControllerTest.java
+++ src/test/java/com/task/account/controller/AccountControllerTest.java
@@ -1,11 +1,23 @@
 package com.task.account.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.task.account.dto.AccountDtoConverter;
+import com.task.account.repository.AccountRepository;
+import com.task.account.service.AccountService;
+import com.task.account.service.CustomerService;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Clock;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +32,28 @@ import static org.junit.jupiter.api.Assertions.*;
 @DirtiesContext
 @RunWith(SpringRunner.class)
 class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Clock clock;
+
+    @MockBean
+    private Supplier<UUID> uuidSupplier;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private AccountDtoConverter accountDtoConverter;
+
+    private AccountService service = new AccountService(accountRepository, customerService, accountDtoConverter);
+    private ObjectMapper mapper = new ObjectMapper();
+    private static final UUID uuid = UUID.randomUUID();
+
 
 }
